498faa39dc212012c802b28acc968175dc19540b
Naveen G <naveeng2402@gmail.com>
2023-04-29 13:49:40 +0530

    Added auto imports

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -4,9 +4,17 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({ dts: true, imports: ["vue"], dirs: ["composables/**"] }),
+    Components({ dts: true, dirs: ["components"], deep: true }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./", import.meta.url)),
